da871d4fc32871e970e35f2abfb5f7e741e80903
Thariq Ivan <108126947+vannndar@users.noreply.github.com>
2024-12-11 20:58:31 +0700

    fix: page

--- frontend-nextjs/app/reservation/[id]/page.tsx
+++ frontend-nextjs/app/reservation/[id]/page.tsx
@@ -108,7 +108,7 @@ const RoomDetails = () => {
 
   const formatDateApi = (dateString: string) => {
     const date = new Date(dateString);
-    return ${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getDate().toString().padStart(2,'0')};
+    return `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')}`;
   };
 
   const formatDate = (dateString: string) => {
